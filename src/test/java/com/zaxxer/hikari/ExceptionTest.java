366fd779a694e8a6fd8a331af889ab52dfc8df7c
leusonmario <leusonmario@gmail.com>
2020-02-04 11:58:15 +0100

    'Trigger build #ca526c2e'

--- src/test/java/com/zaxxer/hikari/ExceptionTest.java
+++ src/test/java/com/zaxxer/hikari/ExceptionTest.java
@@ -1,92 +1,65 @@
 package com.zaxxer.hikari;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import org.junit.Assert;
 import org.junit.Test;
-
-public class ExceptionTest
-{
-    @Test
-    public void testException1() throws SQLException
-    {
-        HikariConfig config = new HikariConfig();
-        config.setMinimumPoolSize(1);
-        config.setMaximumPoolSize(2);
-        config.setAcquireIncrement(1);
-        config.setConnectionTestQuery("VALUES 1");
-        config.setDataSourceClassName("com.zaxxer.hikari.mocks.StubDataSource");
-
-        HikariDataSource ds = new HikariDataSource(config);
-
-        Assert.assertSame("Totals connections not as expected", 1, ds.pool.getTotalConnections());
-        Assert.assertSame("Idle connections not as expected", 1, ds.pool.getIdleConnections());
-
-        Connection connection = ds.getConnection();
-        Assert.assertNotNull(connection);
-
-        Assert.assertSame("Totals connections not as expected", 1, ds.pool.getTotalConnections());
-        Assert.assertSame("Idle connections not as expected", 0, ds.pool.getIdleConnections());
-
-        PreparedStatement statement = connection.prepareStatement("SELECT some, thing FROM somewhere WHERE something=?");
-        Assert.assertNotNull(statement);
-
-        ResultSet resultSet = statement.executeQuery();
-        Assert.assertNotNull(resultSet);
-
-        try
-        {
-            statement.getMaxFieldSize();
-            Assert.fail();
-        }
-        catch (Exception e)
-        {
-            Assert.assertSame(SQLException.class, e.getClass());
-        }
-
-        connection.close();
-
-        Assert.assertSame("Totals connections not as expected", 0, ds.pool.getTotalConnections());
-        Assert.assertSame("Idle connections not as expected", 0, ds.pool.getIdleConnections());
+public class ExceptionTest {
+  @Test public void testException1() throws SQLException {
+    HikariConfig config=new HikariConfig();
+    config.setMinimumPoolSize(1);
+    config.setMaximumPoolSize(2);
+    config.setAcquireIncrement(1);
+    config.setConnectionTestQuery("VALUES 1");
+    config.setDataSourceClassName("com.zaxxer.hikari.mocks.StubDataSource");
+    HikariDataSource ds=new HikariDataSource(config);
+    Assert.assertSame("Totals connections not as expected",1,ds.pool.getTotalConnections());
+    Assert.assertSame("Idle connections not as expected",1,ds.pool.getIdleConnections());
+    Connection connection=ds.getConnection();
+    Assert.assertNotNull(connection);
+    Assert.assertSame("Totals connections not as expected",1,ds.pool.getTotalConnections());
+    Assert.assertSame("Idle connections not as expected",0,ds.pool.getIdleConnections());
+    PreparedStatement statement=connection.prepareStatement("SELECT some, thing FROM somewhere WHERE something=?");
+    Assert.assertNotNull(statement);
+    ResultSet resultSet=statement.executeQuery();
+    Assert.assertNotNull(resultSet);
+    try {
+      statement.getMaxFieldSize();
+      Assert.fail();
     }
-
-    @Test
-    public void testUseAfterClose() throws SQLException
-    {
-        HikariConfig config = new HikariConfig();
-        config.setMinimumPoolSize(1);
-        config.setMaximumPoolSize(2);
-        config.setAcquireIncrement(1);
-        config.setConnectionTestQuery("VALUES 1");
-        config.setDataSourceClassName("com.zaxxer.hikari.mocks.StubDataSource");
-
-        HikariDataSource ds = new HikariDataSource(config);
-
-        Assert.assertSame("Totals connections not as expected", 1, ds.pool.getTotalConnections());
-        Assert.assertSame("Idle connections not as expected", 1, ds.pool.getIdleConnections());
-
-        Connection connection = ds.getConnection();
-        Assert.assertNotNull(connection);
-
-        Assert.assertSame("Totals connections not as expected", 1, ds.pool.getTotalConnections());
-        Assert.assertSame("Idle connections not as expected", 0, ds.pool.getIdleConnections());
-
-        connection.close();
-
-        try
-        {
-            connection.prepareStatement("SELECT some, thing FROM somewhere WHERE something=?");
-            Assert.fail();
-        }
-        catch (SQLException e)
-        {
-            Assert.assertSame("Connection is closed", e.getMessage());
-        }
-
-        Assert.assertSame("Totals connections not as expected", 1, ds.pool.getTotalConnections());
-        Assert.assertSame("Idle connections not as expected", 1, ds.pool.getIdleConnections());
+ catch (    Exception e) {
+      Assert.assertSame(SQLException.class,e.getClass());
     }
+    connection.close();
+    Assert.assertSame("Totals connections not as expected",0,ds.pool.getTotalConnections());
+    Assert.assertSame("Idle connections not as expected",0,ds.pool.getIdleConnections());
+  }
+  @Test public void testUseAfterClose() throws SQLException {
+    HikariConfig config=new HikariConfig();
+    config.setMinimumPoolSize(1);
+    config.setMaximumPoolSize(2);
+    config.setAcquireIncrement(1);
+    config.setConnectionTestQuery("VALUES 1");
+    config.setDataSourceClassName("com.zaxxer.hikari.mocks.StubDataSource");
+    HikariDataSource ds=new HikariDataSource(config);
+    Assert.assertSame("Totals connections not as expected",1,ds.pool.getTotalConnections());
+    Assert.assertSame("Idle connections not as expected",1,ds.pool.getIdleConnections());
+    Connection connection=ds.getConnection();
+    Assert.assertNotNull(connection);
+    Assert.assertSame("Totals connections not as expected",1,ds.pool.getTotalConnections());
+    Assert.assertSame("Idle connections not as expected",0,ds.pool.getIdleConnections());
+    connection.close();
+    try {
+      connection.prepareStatement("SELECT some, thing FROM somewhere WHERE something=?");
+      Assert.fail();
+    }
+ catch (    SQLException e) {
+      Assert.assertSame("Connection is closed",e.getMessage());
+    }
+    Assert.assertSame("Totals connections not as expected",1,ds.pool.getTotalConnections());
+    Assert.assertSame("Idle connections not as expected",1,ds.pool.getIdleConnections());
+  }
+  public ExceptionTest(){
+  }
 }
